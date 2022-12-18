392f655ea1f470a7839f9be087ad0d665f31206b
habibashi <habibashi187@gmail.com>
2022-12-18 14:42:30 +0200

    overlays in index and setState for close and open modal in App.jsx

--- React-Meal/src/App.jsx
+++ React-Meal/src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
-
+import Cart from "./Components/Cart/Cart";
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
