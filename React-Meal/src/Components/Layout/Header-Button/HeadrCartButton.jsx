f4fede32b5af995a6e454bb14e5cb8794b902e12
habibashi <habibashi187@gmail.com>
2022-12-19 21:42:53 +0200

    whtle every click Add meal bump by useEffect hook

--- React-Meal/src/Components/Layout/Header-Button/HeadrCartButton.jsx
+++ React-Meal/src/Components/Layout/Header-Button/HeadrCartButton.jsx
@@ -1,15 +1,41 @@
+import { useContext, useEffect, useState } from 'react';
 import classes from './HeaderCartButton.Module.css';
 import CartIcon from '../../Cart/CartIcon';
+import CartContext from '../../../store/Cart-context';
+
 const HeaderCartButton = ({ onClick }) => {
+    const [btnIsHighlighted, setbtnIsHighlighted] = useState(false);
+    const cartCtx = useContext(CartContext);
+
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setbtnIsHighlighted(true);
+        const timer = setTimeout(() => {
+            setbtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={onClick} >
+        <button className={btnClasses} onClick={onClick} >
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Card</span>
-            <span className={classes.badge}>
-                3
-            </span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     )
 };
